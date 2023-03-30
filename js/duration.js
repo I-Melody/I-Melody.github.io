aba46ee74216cfbefe944443a0ae43ad5587a17f
melody <915128670@qq.com>
2023-03-30 17:52:53 +0800

    Site updated: 2023-03-30 17:52:51

---
+++ js/duration.js
@@ -0,0 +1,31 @@
+!(function() {
+  /** 计时起始时间，自行修改 **/
+  var start = new Date("2022/01/16 20:23:24");
+
+  function update() {
+    var now = new Date();
+    now.setTime(now.getTime()+250);
+    days = (now - start) / 1000 / 60 / 60 / 24;
+    dnum = Math.floor(days);
+    hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+    hnum = Math.floor(hours);
+    if(String(hnum).length === 1 ){
+      hnum = "0" + hnum;
+    }
+    minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+    mnum = Math.floor(minutes);
+    if(String(mnum).length === 1 ){
+      mnum = "0" + mnum;
+    }
+    seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+    snum = Math.round(seconds);
+    if(String(snum).length === 1 ){
+      snum = "0" + snum;
+    }
+    document.getElementById("timeDate").innerHTML = "&nbsp"+dnum+"&nbsp天";
+    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒之前，Melody&nbsp把这个小纸箱放在了这里";
+  }
+
+  update();
+  setInterval(update, 1000);
+})();
